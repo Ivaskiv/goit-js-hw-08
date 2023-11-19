72518bc6af094b20852282159876ac1b468abc69
vira <viraivaskiv@gmail.com>
2023-11-19 20:51:21 +0200

    02-update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -48,12 +48,12 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 // Відключити favicon.ico
-document.addEventListener('DOMContentLoaded', function () {
-  var link =
-    document.querySelector("link[rel*='icon']") ||
-    document.createElement('link');
-  link.type = 'image/x-icon';
-  link.rel = 'shortcut icon';
-  link.href = 'data:image/x-icon;,';
-  document.getElementsByTagName('head')[0].appendChild(link);
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//   var link =
+//     document.querySelector("link[rel*='icon']") ||
+//     document.createElement('link');
+//   link.type = 'image/x-icon';
+//   link.rel = 'shortcut icon';
+//   link.href = 'data:image/x-icon;,';
+//   document.getElementsByTagName('head')[0].appendChild(link);
+// });
